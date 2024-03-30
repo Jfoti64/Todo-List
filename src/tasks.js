99f943026a25083472788e8391ff7d7a3a979f8f
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-03-30 03:13:14 -0500

    Add tasks constructor.

--- src/tasks.js
+++ src/tasks.js
@@ -1,5 +1,11 @@
 class Tasks {
-    constructor(title, ) {
-
+    constructor(title, description, dueDate, completionStatus, important) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.completionStatus = completionStatus;
+        this.important = important;
     }
+
+    
 }
