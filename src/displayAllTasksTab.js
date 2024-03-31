84a2b17aee5e28420ab74b7d1ef5377defb63a51
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-03-31 18:06:02 -0500

    Add description and dueDate to dom dynamically.

--- src/displayAllTasksTab.js
+++ src/displayAllTasksTab.js
@@ -27,6 +27,19 @@ function createNewTaskCard(obj) {
     title.innerHTML = obj.title;
     taskCard.appendChild(title);
 
+    // Create description
+    const description = document.createElement('p');
+    description.classList.add('description');
+    description.innerHTML = obj.description;
+    taskCard.appendChild(description);
+
+    // Create dueDate
+    const dueDate = document.createElement('p');
+    dueDate.classList.add('dueDate');
+    dueDate.innerHTML = obj.dueDate;
+    taskCard.appendChild(dueDate);
+
+
     tasksContainer.appendChild(taskCard);
 }
 
